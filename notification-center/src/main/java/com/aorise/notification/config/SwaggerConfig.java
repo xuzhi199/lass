cd22fe9f8b7229821f74189944d99e1e995f7f5c
xuzhi <xuzhi@umxwe.com>
2021-02-25 15:58:50 +0800

    更新swagger文档head参数

--- notification-center/src/main/java/com/aorise/notification/config/SwaggerConfig.java
+++ notification-center/src/main/java/com/aorise/notification/config/SwaggerConfig.java
@@ -3,11 +3,17 @@ package com.aorise.notification.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * swagger文档
@@ -21,9 +27,16 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket docket() {
+		//添加header参数
+		ParameterBuilder ticketPar = new ParameterBuilder();
+		List<Parameter> pars = new ArrayList<>();
+		ticketPar.name("Authorization").description("user token")
+				.modelRef(new ModelRef("string")).parameterType("header")
+				.required(false).build(); //header中的ticket参数非必填，传空也可以
+		pars.add(ticketPar.build());    //根据每个方法名也知道当前方法在设置什么参数
 		return new Docket(DocumentationType.SWAGGER_2).groupName("通知中心swagger接口文档")
 				.apiInfo(new ApiInfoBuilder().title("通知中心swagger接口文档")
 						.contact(new Contact("blues199", "", "blues199@163.com")).version("1.0").build())
-				.select().paths(PathSelectors.any()).build();
+				.select().paths(PathSelectors.any()).build().globalOperationParameters(pars);
 	}
 }
